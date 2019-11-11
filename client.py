8b91a341a8eb0e62a39855d6650c9c2aa2f6c36a
YTGerd <ytgerd1995@gmail.com>
2019-11-11 21:56:39 +0100

    new

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+import Distance as lp
+#creat a tcp/ip socket
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+t=lp.getCharacteristic()
+print(t)
+#connect the socket to the port where the server is listening
+server_address=('localhost',8080)
+print('connecting to %s port %s' % server_address)
+sock.connect(server_address)
+
+
+try:
+    message=t
+    print("sending %s" % message)
+    sock.sendall(message.encode())
+
+    #look for the response
+    amount_received=0
+    amount_expected=len(message)
+
+    while amount_received<amount_expected:
+        data=sock.recv(1024)
+        amount_received+=len(data)
+        print("received %s" % data.decode())
+finally:
+    print("closing socket")
+    sock.close()
+
---
+++ server.py
@@ -0,0 +1,34 @@
+import socket 
+import sys
+
+#creat a tcp/ip socket
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#bind the socket to the port
+server_address=('',8080)
+print("starting up on %s poot %s" % server_address)
+sock.bind(server_address)
+
+#listen fir incomming connections
+sock.listen(1)
+
+while True:
+    #wait for connection
+    print("waiting for a connection.....")
+    connection,client_address=sock.accept()
+
+    try:
+        print("connection from",client_address)
+        while True:
+            data=connection.recv(1024)
+            print("received %s" % data.decode())
+            if data:
+                print("sending data back to the clien")
+                connection.sendall(data)
+            else:
+                print("no more data from",client_address)
+                break
+    finally:
+        connection.close()
+
+    
